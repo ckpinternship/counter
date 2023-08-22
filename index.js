ea574c9c011d4e69308a37a64321203cf06b72ec
Shaked Apter <shakedap@checkpoint.com>
2023-08-22 23:18:55 +0300

    Added water update from the JS
    Added two main variables: current_services that holds the current developed services and target_services that holds the target amount

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const current_services = 80;
+const target_services = 80;
+
+// -------- source --------
 const wave = document.getElementById("wave");
 
-wave.style.top = "50%";
+// The higher the number, the lower the liquid
+var current_amount = 100 - Math.round(current_services / target_services * 100);
+wave.style.top = current_amount.toString() + "%";
