f9ae4c4bb85924320a63dcded27c1b293783009b
Shaked Apter <shakedap@checkpoint.com>
2023-08-23 18:48:48 +0300

    Add update from js #4

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const target_services = 80;
 
 // -------- source --------
 const wave = document.getElementById("wave");
+const number = document.getElementById("number");
 
 // The higher the number, the lower the liquid
 var current_amount = 100 - Math.round(current_services / target_services * 100);
 wave.style.top = current_amount.toString() + "%";
+
+number.innerText = current_services.toString();
