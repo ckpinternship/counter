b04fe1638efa3a8624453f8cb6a929cfd1793281
Shaked Apter <shakedap@checkpoint.com>
2023-08-23 20:44:39 +0300

    Add different height level of liquid, and originize assets to folder
    Move logo and images to "/assets"
    Add different images with different wave heights
    Add js code to randomly set the image

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const wave = document.getElementById("wave");
 const number = document.getElementById("number");
 
 // The higher the number, the lower the liquid
+// update liquid amound and number
 var current_amount = 100 - Math.round(current_services / target_services * 100);
 wave.style.top = current_amount.toString() + "%";
-
 number.innerText = current_services.toString();
+
+// update liquid wave height
+var current_coffee = 1;
+if (100-current_amount <= 4)
+    current_coffee = 6;
+else if (100-current_amount <= 10)
+    current_coffee = Math.floor(Math.random() * 3) + 4;
+else 
+    current_coffee = Math.floor(Math.random() * 4) + 2;
+wave.style.backgroundImage = `url(assets/coffee${current_coffee}.png)`
